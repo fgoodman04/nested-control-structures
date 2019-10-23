7887268bf6c55e1697b204ae55f38e56ffffc49a
Faith Goodman <02faith.goodman@gulllakecs.org>
2019-10-23 13:56:58 -0400

    Initial Commit for For Loop + While Loop

--- Nested Control Structures.py
+++ Nested Control Structures.py
@@ -8,4 +8,19 @@ This program will nest a for loop inside another for loop.
 for i in range(3):
     print("Outer For Loop: " + str(i))
     for y in range(2):
+<<<<<<< Updated upstream
         print("   Inner For Loop: " + str(y))
+=======
+        print("   Inner For Loop: " + str(y))
+
+print("\n                                                                  \n")
+
+"""
+Programmer: Faith Goodman
+Date: 10.23.19
+Program: For Loop plus While Loop
+
+This program will create a For Loop with a While Loop embedded into it.
+"""
+
+
