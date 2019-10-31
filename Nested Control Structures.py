8c8ce8968228333fc90505a9452499b53ddc8d77
fgoodman04 <56651094+fgoodman04@users.noreply.github.com>
2019-10-31 13:53:28 -0400

    Update Nested Control Structures.py

--- Nested Control Structures.py
+++ Nested Control Structures.py
@@ -20,7 +20,11 @@ Programmer: Faith Goodman
 Date: 10.23.19
 Program: For Loop plus While Loop
 
-This program will create a For Loop with a While Loop embedded into it.
+This program will create a For Loop with a While Loop embedded into it
 """
-
-
+for i in range(4):
+    print("Outer For Loop: " + str(i))
+    x = 5
+    while x >= 0:
+        print("         While Loop: " + str(x))
+        x = x - 1
